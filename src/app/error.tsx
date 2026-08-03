291cb5bf1ae12527aeb35f86b52424eeab5eeba7
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-08-03 21:53:15 +0600

    refactor: update Error component structure and styling; enhance readability and consistency

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,21 +1,22 @@
 "use client";
 
-export default function GlobalError({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
+interface Props {
+  error: Error;
   reset: () => void;
-}) {
+}
+
+export default function Error({ reset }: Props) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h2 className="text-2xl font-bold">Something went wrong!</h2>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-5">
+      <h1 className="text-3xl font-bold">
+        Something went wrong!
+      </h1>
 
       <button
-        onClick={() => reset()}
-        className="rounded-md bg-black px-4 py-2 text-white"
+        onClick={reset}
+        className="rounded bg-black px-5 py-2 text-white"
       >
-        Try again
+        Try Again
       </button>
     </div>
   );
